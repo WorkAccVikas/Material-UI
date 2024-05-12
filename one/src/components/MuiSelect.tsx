2eff1c79c76b3e3b5cee3986888fe33600ae5fed
WorkAccVikas <workaccvikas@gmail.com>
2024-05-13 00:47:01 +0530

    c8

--- one/src/components/MuiSelect.tsx
+++ one/src/components/MuiSelect.tsx
@@ -1,10 +1,20 @@
-import { Box, MenuItem, TextField } from "@mui/material";
+import {
+  Box,
+  FormControl,
+  InputLabel,
+  MenuItem,
+  Select,
+  SelectChangeEvent,
+  Stack,
+  TextField,
+} from "@mui/material";
 import React, { useState } from "react";
 
 const MuiSelect = () => {
   const [country, setCountry] = useState("");
   const [countries, setCountries] = useState<string[]>([]);
-  console.log({ countries });
+  const [age, setAge] = useState("");
+  //   console.log({ countries });
 
   const handleChange = (e: React.ChangeEvent<HTMLInputElement>) => {
     setCountry(e.target.value);
@@ -16,38 +26,55 @@ const MuiSelect = () => {
     setCountries(typeof value === "string" ? value.split(",") : value);
   };
 
+  const handleChange2 = (e: SelectChangeEvent<string>) => {
+    setAge(e.target.value);
+  };
+
   return (
     <>
-      <Box width="250px">
-        {/* Single Select */}
-        <TextField
-          label="Select Country"
-          select
-          fullWidth
-          value={country}
-          onChange={handleChange}
-        >
-          <MenuItem value="IN">India</MenuItem>
-          <MenuItem value="PAK">Pakistan</MenuItem>
-          <MenuItem value="US">USA</MenuItem>
-        </TextField>
+      <Stack spacing={4}>
+        <Box width="250px">
+          {/* Single Select */}
+          <TextField
+            label="Select Country"
+            select
+            fullWidth
+            value={country}
+            onChange={handleChange}
+          >
+            <MenuItem value="IN">India</MenuItem>
+            <MenuItem value="PAK">Pakistan</MenuItem>
+            <MenuItem value="US">USA</MenuItem>
+          </TextField>
 
-        {/* Multiple Select */}
-        <TextField
-          label="Select Country"
-          select
-          fullWidth
-          value={countries}
-          onChange={handleChange1}
-          SelectProps={{
-            multiple: true,
-          }}
-        >
-          <MenuItem value="IN">India</MenuItem>
-          <MenuItem value="PAK">Pakistan</MenuItem>
-          <MenuItem value="US">USA</MenuItem>
-        </TextField>
-      </Box>
+          {/* Multiple Select */}
+          <TextField
+            label="Select Country"
+            select
+            fullWidth
+            value={countries}
+            onChange={handleChange1}
+            SelectProps={{
+              multiple: true,
+            }}
+          >
+            <MenuItem value="IN">India</MenuItem>
+            <MenuItem value="PAK">Pakistan</MenuItem>
+            <MenuItem value="US">USA</MenuItem>
+          </TextField>
+        </Box>
+
+        <Box width="250px">
+          <FormControl fullWidth>
+            <InputLabel>Age</InputLabel>
+            <Select label="age" value={age} onChange={handleChange2} fullWidth>
+              <MenuItem value={10}>Ten</MenuItem>
+              <MenuItem value={20}>Twenty</MenuItem>
+              <MenuItem value={30}>Thirty</MenuItem>
+            </Select>
+          </FormControl>
+        </Box>
+      </Stack>
     </>
   );
 };
